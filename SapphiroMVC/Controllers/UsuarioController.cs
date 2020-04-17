0855885561ebba985e907c6cb6bb5faf6d2486a7
ltramos14 <48187187+ltramos14@users.noreply.github.com>
2020-04-16 22:27:21 -0500

    Login Fallido

---
+++ SapphiroMVC/Controllers/UsuarioController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Npgsql;
+using SapphiroMVC.Models;
+
+
+namespace SapphiroMVC.Controllers
+{
+    public class UsuarioController : Controller
+    {
+
+        NpgsqlConnection conexion = new NpgsqlConnection();
+        NpgsqlCommand comando = new NpgsqlCommand();
+        NpgsqlDataReader dr;
+        // GET: Usuario
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        void connectionString()
+        {
+            conexion.ConnectionString = "UserID = postgres; Password = 0609; host = localhost; Port=5432; Database=bdsapphiro;";
+        }
+
+        [HttpPost]
+        public ActionResult Verify(Usuario usu)
+        {
+            connectionString();
+            conexion.Open();
+            comando.Connection = conexion;
+            comando.CommandText = "SELECT * FROM \"bdsapphiro\".\"Usuario\" WHERE nombre_usuario='" + usu.nombre_usuario + "'AND contraseña='" + usu.contraseña + "'";
+            dr = comando.ExecuteReader();
+
+            if (dr.Read())
+            {
+                conexion.Close();
+                return View("CuentaCreada");
+            }
+            else
+            {
+                conexion.Close();
+                return View("Error");
+            }
+        }
+
+    }
+}
--- SapphiroMVC/Models/Usuario.cs
+++ SapphiroMVC/Models/Usuario.cs
@@ -7,5 +7,14 @@ namespace SapphiroMVC.Models
 {
     public class Usuario
     {
+        public int id_usuario { get; set; }
+        public int id_rol { get; set; }
+        public string nombre_usuario { get; set; }
+        public string contraseña { get; set; }
+        public string primer_nombre { get; set; }
+        public string segundo_nombre { get; set; }
+        public string primer_apellido { get; set; }
+        public string segundo_apellido { get; set; }
+        public string nombre_empresa { get; set; }
     }
 }
